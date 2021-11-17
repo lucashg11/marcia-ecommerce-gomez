2e3a20e405839a66bfb0eef020ce7c89d953c9c1
Lucas Gomez <gomezlucashernan@hotmail.com>
2021-11-17 12:33:34 -0300

    Ultimas modificaciones

--- BrandName.jsx
+++
@@ -1,7 +0,0 @@
-import './Components.scss';
-
-export const BrandName = ()=> {
-    return (
-        <p className="brand">Marcia</p>
-    )
-}
--- src/containers/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer.jsx
@@ -13,22 +13,13 @@ export const ItemDetailContainer = ()=>{
             .then(res=>{res.json(); console.log(res.json())})
             .then(data=>setItem(data))
 
-<<<<<<< HEAD
+
     }, [itemId])
     
     return(
         <div>
             {item ? <ItemDetail itemId={item.id}/> : "Cargando..."}
         </div>
-=======
-   return (
-       <div className="itemDetailContainer">
-           {productDetail?.filter((product)=>product.id===1)
-           .map((product, id)=>(
-               <ItemDetail item={product} key={id}/>
-           ))}
-       </div>
->>>>>>> 4dba3aa981c48c8530851e0c917afb1737f6bc4d
     )
 
        
